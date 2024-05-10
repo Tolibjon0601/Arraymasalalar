fae99f03aa55987d1e84819cec8b3a35d364526f
Tolibjon0601 <egamberdiyevtolibjon0601@gmail.com>
2024-05-10 23:54:33 +0500

    end

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-let array = [1, 2, 3, 4, 5,6];
+// 1-masala
 
-console.log("Original Array: ");
-console.log(array);
+// let inputString =prompt("Harflarni kiriting: ");
+// function letters(str) {
+//  let result = "";
+//  for (let i = 0; i < str.length; i++) {
+//      result += str[i].repeat(i + 1) + "-";
+//  }
+//  return result.slice(0, -1);
+// }
 
-array.reverse();
+// let natija = letters(inputString);
+// console.log(natija);
 
-console.log("Reverse Array: ");
-console.log(array );
+// 2-masala
+
+// function lettersReturned(str) {
+//  return /[a-zA-Z]/.test(str);
+// }
+
+// let inputString = "tolibjon";
+// let result = lettersReturned(inputString);
+// console.log(result);
+
+// 3-masala
+
+// let numbers = [1, 2, 3, 4, 5];
+// function isSumEven(array) {
+// 	let sum = 0;
+// 	for (let i = 0; i < array.length; i++) {
+// 		sum += array[i];
+// 	}
+// 	return sum % 2 === 0;
+// }
+// let isEven = isSumEven(numbers);
+// console.log(isEven);
+
+
+// 4-masala
+
